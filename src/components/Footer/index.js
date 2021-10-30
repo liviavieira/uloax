d327d400a0045130122da93a430b751426b4ea2c
IanAndrade2003 <ianandrade90@outlook.com>
2021-10-29 22:00:57 -0300

    incomplete footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -49,6 +49,46 @@ export default function Footer() {
   const query = data.uloaxdata.footers[0];
   
   return (
-    <h2>Footer</h2>
+<S.Container>
+      <S.Boxaddress>
+        <S.Titleaddress>{query.address}</S.Titleaddress>
+        <S.Paragraphaddress>{query.lorem}</S.Paragraphaddress>
+      </S.Boxaddress>
+      <S.Boxlinks>
+        <S.Titlelinks>{query.links}</S.Titlelinks>
+        <div>
+          <S.Linkimg src={query.imglist.url}/>
+          <S.Paragraphlinks>{query.home}</S.Paragraphlinks>
+          <S.Linkimg src={query.imglist.url}/>
+          <S.Paragraphlinks>{query.about}</S.Paragraphlinks>
+          <S.Linkimg src={query.imglist.url}/>
+          <S.Paragraphlinks>{query.taxi}</S.Paragraphlinks>
+          <S.Linkimg src={query.imglist.url}/>
+          <S.Paragraphlinks>{query.booking}</S.Paragraphlinks>
+          <S.Linkimg src={query.imglist.url}/>
+          <S.Paragraphlinks>{query.contact}</S.Paragraphlinks>
+        </div>
+      </S.Boxlinks>
+      <div>
+        <S.Titlefollow>{query.follow}</S.Titlefollow>
+        <div>
+          <S.Followimg src={query.facebookicon.url}/>
+          <S.Paragraphfollow>{query.facebook}</S.Paragraphfollow>
+          <S.Followimg src={query.twittericon.url}/>
+          <S.Paragraphfollow>{query.twitter}</S.Paragraphfollow>
+          <S.Followimg src={query.linkedinicon.url}/>
+          <S.Paragraphfollow>{query.linkedin}</S.Paragraphfollow>
+          <S.Followimg src={query.youtubeicon.url}/>
+          <S.Paragraphfollow>{query.youtube}</S.Paragraphfollow>
+          <S.Followimg src={query.instagramicon.url}/>
+          <S.Paragraphfollow>{query.instagram}</S.Paragraphfollow>
+        </div>
+      </div>
+      <S.Boxnew>
+        <S.Titlenew>{query.newsletter}</S.Titlenew>
+        <S.Inputnew type="email" placeholder="Enter Your Email"/>
+        <S.btn>{query.subscribe}</S.btn>
+      </S.Boxnew>
+    </S.Container>
   );
 }
