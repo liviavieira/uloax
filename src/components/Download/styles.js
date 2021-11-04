9a262130cece2ac4b04258eb8514db01f0a6a440
liviavieira <vieira.liviaaa@gmail.com>
2021-11-04 02:22:55 -0300

    feat download section

--- src/components/Download/styles.js
+++ src/components/Download/styles.js
@@ -1,2 +1,34 @@
 import styled from "styled-components";
 
+export const Container = styled.section`
+  display: flex;
+  justify-content: flex-end;
+  height: calc(100vh - 4.2rem);
+  background: url(${props => props.bg});
+  background-size: 100%;
+  background-repeat: no-repeat;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 7vw 3rem 1rem;
+  width: 65%;
+`;
+
+export const Title = styled.h2`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-top: 1rem;
+  height: 10rem;
+  color: #24292b;
+  font: 300 3em sans-serif;
+
+  span {
+    color: #fff;
+    font-size: .95em;
+  }
+`;
+
+export const Phone = styled.img``;
